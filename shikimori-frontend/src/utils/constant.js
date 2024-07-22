ce2056bee079660fc12ba6a7d7fe0f9595945eb6
Ankur-Raphacure <ankur@raphacure.com>
2024-07-22 13:17:16 +0530

    dynamic api support

--- shikimori-frontend/src/utils/constant.js
+++ shikimori-frontend/src/utils/constant.js
@@ -1,1 +1,7 @@
-export const API_URL = "http://localhost:8000";
+const NODE_ENV = process.env.NODE_ENV;
+let API_Link =
+  NODE_ENV === "production"
+    ? "https://shikimori-api.vercel.app"
+    : "http://localhost:8000";
+
+export const API_URL = API_Link;
